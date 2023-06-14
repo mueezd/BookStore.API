201a4f3f0d6dd77e49c8691942c9fce45fce34c0
MUEEZ R DEEPRO <deepro.asl@gmail.com>
2023-06-14 16:58:03 +0600

    Add project files.

---
+++ BookStore.API/Data/BookStoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Data
+{
+    public class BookStoreDbContext: DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Books> Books { get; set; }
+
+
+    }
+}
